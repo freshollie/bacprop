67933c52c20478b6f6454311824c7b23fb4f316a
Oliver Bell <freshollie@gmail.com>
2018-10-24 12:26:19 +0100

    testing bacnet integration

--- bacprop/service.py
+++ bacprop/service.py
@@ -1,23 +1,43 @@
 import asyncio
+import json
+from typing import Dict
 from hbmqtt.client import MQTTClient, ClientException
 from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
 class BacPropergator:
+
+    TOPIC_START_FORMAT = "sensor-"
+
     def __init__(self):
         self._loop = asyncio.get_event_loop()
+
+    async def _handle_sensor_data(self, data: Dict[str, str]):
+        pass
     
     async def _run(self):
         C = MQTTClient()
         await C.connect('mqtt://localhost')
-        # Subscribe to '$SYS/broker/uptime' with QOS=1
-        # Subscribe to '$SYS/broker/load/#' with QOS=2
+        
         await C.subscribe([
-                ('#', QOS_1),
+                (f'{BacPropergator.TOPIC_START_FORMAT}#', QOS_1),
             ])
+
+        print("MQTT listening")
         try:
             while True:
+                # Wait for a message to come from any of the topics
                 message = await C.deliver_message()
                 packet = message.publish_packet
+
+                # We expect the topic to start with "sensor"
+                try:
+                    data = str(packet.payload.data)
+                    data = json.loads(data)
+                except json.JSONDecodeError:
+                    # Bad data
+                    continue
+
+                await self._handle_sensor_data(data)
                 print("%s => %s" % (packet.variable_header.topic_name, str(packet.payload.data)))
             await C.disconnect()
         except ClientException as ce:
@@ -25,4 +45,5 @@ class BacPropergator:
     
     def run(self):
         self._loop.run_until_complete(self._run())
+
 
